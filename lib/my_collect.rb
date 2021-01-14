2c8984099accf9c057275643142728887fab4cf5
figo5530 <figooooo0221@gmail.com>
2021-01-13 20:23:59 -0500

    commit tree f5d2eaffaadcebce85a214295a94bb65d3e9d696

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+    count = 0
+    collection = []
+    if array.size == 0 
+        return count
+    end
+    while count < array.size
+        collection << yield(array[count])
+        count += 1
+    end
+    collection
+end
 
